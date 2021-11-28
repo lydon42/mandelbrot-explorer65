2639e30a39fe87a12d23039385e485d2895f84bd
Oliver Graf <ograf@devilsclaw.de>
2021-11-28 22:43:27 +0100

    palette using seaborn

--- palgen.py
+++ palgen.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python
 
+# https://seaborn.pydata.org/tutorial/color_palettes.html
+# -> use seaborn.color_palette directly!
+
 import sys
 from PIL import Image
+import seaborn as sns
 
 colors = int(sys.argv[1])
 reverse = colors<0
 colors = abs(colors)
-img = Image.open(sys.argv[2])
-
-xpart = img.size[0]//colors
-x = (img.size[0] - xpart*colors)//2
-y = img.size[1]//2
-
+#img = Image.open(sys.argv[2])
+pal = sns.color_palette("magma", as_cmap=True)
 if reverse:
-    xpart = -xpart
-    x = img.size[0]-x
+    pal = pal.reversed()
+
+xpart = pal.N//colors
+x = (pal.N - xpart*colors)//2
 
 r, g, b = [], [], []
 for i in range(colors):
-    col = img.getpixel((x+xpart*i,y))
+    col = [int(x*255) for x in pal.colors[x+xpart*i]]
     r.append(col[0]>>4 | ((col[0]<<4)&0xf0))
     g.append(col[1]>>4 | ((col[1]<<4)&0xf0))
     b.append(col[2]>>4 | ((col[2]<<4)&0xf0))
